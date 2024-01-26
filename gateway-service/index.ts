72787c8067cb07a481a39903e86faf5fd6da334d
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-01-26 19:34:34 +0300

    feat: file service is added to gateway

--- gateway-service/index.ts
+++ gateway-service/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRouter from "./Api/Concrete/Routers/UserServiceRouter";
+import fileRouter from "./Api/Concrete/Routers/FileServiceRouter";
 
 dotenv.config();
 
 const app = express();
 
 app.use(userRouter);
+app.use(fileRouter);
 
 const PORT = process.env.PORT || 3000;
 
