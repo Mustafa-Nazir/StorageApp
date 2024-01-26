00661199fb4210d6961741cf9e391d8c28c97ee2
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-01-26 19:27:37 +0300

    feat: api layer is added

--- file-service/index.ts
+++ file-service/index.ts
@@ -1,1 +1,21 @@
+import "reflect-metadata";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`file server is running on port:${PORT}`);
+});
