f6368fefb07e954d07e137a1c9dea8dc5b3caea4
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-02-25 20:24:23 +0300

    feat: cors is added

--- gateway-service/index.ts
+++ gateway-service/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 import userRouter from "./Api/Concrete/Routers/UserServiceRouter";
 import fileRouter from "./Api/Concrete/Routers/FileServiceRouter";
 
@@ -7,10 +9,12 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
+
 app.use(userRouter);
 app.use(fileRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`gateway server is running on port:${PORT}`);
