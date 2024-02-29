7cc3242168948711bcdc3af82451e5eae2dcfce8
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-02-29 22:48:29 +0300

    fix: http method

--- user-service/Api/Concrete/Routers/LibraryRouter.ts
+++ user-service/Api/Concrete/Routers/LibraryRouter.ts
@@ -7,6 +7,6 @@ const router = express.Router();
 const libraryController = container.resolve(LibraryController);
 
 router.post("/add",(req:any , res:any) => {libraryController.Add(req,res);});
-router.post("/addDepartments",(req:any , res:any) => {libraryController.AddDepartments(req,res);});
+router.patch("/addDepartments",(req:any , res:any) => {libraryController.AddDepartments(req,res);});
 
 export default router;
