05d47d36572d777662db7625725b722fec4ee0dd
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-04-04 16:51:03 +0300

    feat: services is added

--- file-service/Api/DependencyResolvers/TSyrnge/IoCContainer.ts
+++ file-service/Api/DependencyResolvers/TSyrnge/IoCContainer.ts
@@ -1,3 +1,22 @@
 import {container} from "tsyringe"
+import IAnnouncementDal from "../../../DataAccess/Abstract/IAnnouncementDal";
+import MsAnnouncementDal from "../../../DataAccess/Concrete/Mongoose/MsAnnouncementDal";
+import IAnnouncementService from "../../../Business/Abstract/IAnnouncementService";
+import AnnouncementManager from "../../../Business/Concrete/AnnouncementManager";
+import AnnouncementController from "../../Concrete/Controllers/AnnouncementController";
+import IFolderDal from "../../../DataAccess/Abstract/IFolderDal";
+import MsFolderDal from "../../../DataAccess/Concrete/Mongoose/MsFolderDal";
+import IFolderService from "../../../Business/Abstract/IFolderService";
+import FolderManager from "../../../Business/Concrete/FolderManager";
+import FolderController from "../../Concrete/Controllers/FolderController";
+
+container.registerSingleton<IAnnouncementDal>("IAnnouncementDal",MsAnnouncementDal);
+container.registerSingleton<IAnnouncementService>("IAnnouncementService",AnnouncementManager);
+
+container.registerSingleton<IFolderDal>("IFolderDal",MsFolderDal);
+container.registerSingleton<IFolderService>("IFolderService",FolderManager);
+
+container.registerSingleton(AnnouncementController);
+container.registerSingleton(FolderController);
 
 export {container};
--- file-service/index.ts
+++ file-service/index.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import MsStorageAppDb from "./DataAccess/Concrete/Mongoose/MsStorageAppDb";
+import announcementRouter from "./Api/Concrete/Routers/AnnouncementRouter";
+import folderRouter from "./Api/Concrete/Routers/FolderRouter";
 
 dotenv.config();
 
@@ -14,7 +17,12 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/announcement",announcementRouter);
+app.use("/folder",folderRouter);
+
 const PORT = process.env.PORT || 5000;
+
+MsStorageAppDb.DbConnect();
 
 app.listen(PORT, () => {
     console.log(`file server is running on port:${PORT}`);
